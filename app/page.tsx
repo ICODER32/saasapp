2c1de031d69e408adf37aebe263ba001d7cdb695
zaindgr8 <66069515+zaindgr8@users.noreply.github.com>
2023-10-10 13:14:15 +0500

    zain

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,8 @@ import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
-  return <UserButton afterSignOutUrl="/" />;
+  return <>
+  <h1>Zain</h1>
+  <UserButton afterSignOutUrl="/" />
+  </> ;
 }
